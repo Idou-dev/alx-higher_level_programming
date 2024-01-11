9ee9247e8a01ab83c0a47d721305f4a66e03c7ac
idou-dev <idouyoussef87@gmail.com>
2024-01-11 14:33:00 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    l = len(sys.argv)
+    j = len(sys.argv)
     i = 1
     if l < 2:
         print("{} {}".format(l - 1, "arguments."))
     elif l == 2:
         print("{} {}".format(i, "argument:"))
-        while(i < l):
+        while (i < l):
             print("{}: {}".format(l - 1, sys.argv[i]))
             i += 1
     else:
         print("{} {}".format(l - 1, "arguments:"))
-        while(i < l):
+        while (i < l):
             print("{}: {}".format(i, sys.argv[i]))
             i += 1
