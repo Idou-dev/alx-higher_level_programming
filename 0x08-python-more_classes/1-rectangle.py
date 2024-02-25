6653ffe2cf16ebd4383f5f0eaa6a3cfa0caed4b1
idou-dev <idouyoussef87@gmail.com>
2024-02-25 13:31:13 +0000

    set ractangle width and height

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""creates a Rectangle class"""
+
+
+class Rectangle:
+    """Rectagle class"""
+    
+    def __init__(self, width=0, height=0):
+        """__init__ methode
+
+        Args:
+            width: the rectangle width
+            height: the height width
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """property of rectangle width
+
+        Raises:
+            TypeError: if width is not an int
+            ValueError: if width is < 0
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """property of rectangle height
+
+        Raises:
+            TypeError: if height is not an int
+            ValueError: if height < 0
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
