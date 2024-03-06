2d0de8e7fe442e6ecc7b8071520a82252f4ec8b5
idou-dev <idouyoussef87@gmail.com>
2024-03-06 08:06:20 +0000

    base geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""base goemetry module"""
+
+
+class BaseGeometry:
+    """base goemetry class"""
+    def area(self):
+        """raises an Exception"""
+        raise Exception('area() is not implemented')
