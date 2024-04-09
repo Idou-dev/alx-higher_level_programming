4bfaf769a7383c61981109ae45d9bae8a830e63e
idou-dev <idouyoussef87@gmail.com>
2024-04-09 01:44:25 +0000

    rectangle erea and perimeter

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""rectangle module"""
+
+
+class Rectangle:
+    """defines a Rectangle class"""
+    def __init__(self, width=0, height=0):
+        """Instantiation methode"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """retrieves the Private instance attribute: width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets the instance width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """retrieves the Private instance attribute height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets the private instance attribute height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """returns the erea of Rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """returns the perimeter of Rectangle"""
+        return 2 * (self.__height + self.__width)
