7701639ba046fdf88b5a81d950f29cfa0189901c
Idou-dev <idouyoussef87@gmail.com>
2024-04-16 07:37:35 -0700

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""read_file methode"""
+"""read_file method"""
 
 
 def read_file(filename=""):
     """reads a text file and prints it to the stdout"""
-    with open(filename, 'r') as f:
+    with open(filename, encoding="utf-8") as f:
         read_d = f.read()
-    print(read_d)
+    print(read_d, end='')
