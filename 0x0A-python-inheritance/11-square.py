77b9490bca5ff824a477d911c608e20433d44fad
Idou-dev <idouyoussef87@gmail.com>
2024-04-20 06:20:20 -0700

    square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""10-square method"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Square that inherits from Rectangle"""
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """area implementation"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """string representation"""
+        return ("[Square] " + str(self.__size) + "/" + str(self.__size))
