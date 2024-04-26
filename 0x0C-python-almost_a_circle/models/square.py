935c51f7b2cbc7332f085688eb697d341fd68279
idou-dev <idouyoussef87@gmail.com>
2024-04-26 16:32:35 -0700

    square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""square method"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Class Square inherits from Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Class constructor"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """string representation for class square"""
+        return (
+                '[{}] ({}) {}/{} - {}'.format(
+                    type(self).__name__, self.id, self.x, self.y, self.width))
