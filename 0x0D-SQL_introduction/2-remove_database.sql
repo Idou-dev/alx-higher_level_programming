e8c2575f0bd3980fbbc6d19f1848c5ae17663063
idou-dev <idouyoussef87@gmail.com>
2024-05-24 03:58:51 -0700

    remove database

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- script that creates the database hbtn_0c_0 in your MySQL server
 -- If the database hbtn_0c_0 already exists, script should not fail
-DELETE DATABASE IF EXISTS hbtn_0c_0
+DROP DATABASE IF EXISTS hbtn_0c_0
